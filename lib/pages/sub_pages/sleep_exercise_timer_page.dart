f692bc1150cf581989d979265cf768ed9ac8f4c5
Pathum <npathumdilhara@gmail.com>
2024-10-17 10:23:40 +0530

    Timer for sleep story page

--- lib/pages/sub_pages/sleep_exercise_timer_page.dart
+++ lib/pages/sub_pages/sleep_exercise_timer_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:meditator_mobile_app/models/sleep_exercise_model.dart';
 import 'package:meditator_mobile_app/utils/colors.dart';
@@ -14,6 +16,73 @@ class SleepExerciseTimerPage extends StatefulWidget {
 }
 
 class _SleepExerciseTimerPageState extends State<SleepExerciseTimerPage> {
+  Timer? _timer;
+  int _remainingTime = 0;
+  bool _isRunning = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _remainingTime = widget.sleepExerciseModel.duration * 60;
+  }
+
+  // Method to start the timer
+  void _startTimer() {
+    if (_timer != null) {
+      _timer!.cancel();
+    }
+    _isRunning = true;
+    _timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) {
+        setState(
+          () {
+            if (_remainingTime > 0) {
+              _remainingTime--;
+            } else {
+              _timer!.cancel();
+            }
+          },
+        );
+      },
+    );
+  }
+
+  // Method to pause the timer
+  void _pauseTimer() {
+    if (_timer != null) {
+      _timer!.cancel();
+      _isRunning = false;
+    }
+  }
+
+  // Method to stop the timer
+  void _stopTimer() {
+    if (_timer != null) {
+      _timer!.cancel();
+    }
+    setState(() {
+      _remainingTime = widget.sleepExerciseModel.duration * 60;
+    });
+  }
+
+  // Method to time formatter as xx : xx
+  String _timeFormatter(int seconds) {
+    final int minutes =
+        seconds ~/ 60; // truncated division operator to return int
+    final int remainingSeconds = seconds % 60;
+
+    return "$minutes:${remainingSeconds.toString().padLeft(2, '0')}";
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (_timer != null) {
+      _timer!.cancel();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,6 +137,50 @@ class _SleepExerciseTimerPageState extends State<SleepExerciseTimerPage> {
               const SizedBox(
                 height: 30,
               ),
+              Center(
+                child: Text(
+                  _timeFormatter(
+                    _remainingTime,
+                  ),
+                  style: const TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _isRunning ? _pauseTimer() : _startTimer();
+                      });
+                    },
+                    child: Text(_isRunning
+                        ? "Pause"
+                        : _remainingTime <
+                                widget.sleepExerciseModel.duration * 60
+                            ? "Resume"
+                            : "Start"),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: (){
+                      setState(() {
+                        _stopTimer();
+                        _isRunning = false;
+                      });
+                    },
+                    child: const Text("Stop"),
+                  ),
+                ],
+              )
             ],
           ),
         ),
