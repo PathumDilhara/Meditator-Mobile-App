de4acd73e7c68a565f8ca7dc15d22f11674b2545
Pathum <npathumdilhara@gmail.com>
2024-10-31 11:44:13 +0530

    Display & delete meditations and type conversions

--- lib/services/meditation_services.dart
+++ lib/services/meditation_services.dart
@@ -13,8 +13,8 @@ class MeditationServices {
   Future<void> addMeditation(
       MeditationExerciseModel meditation, BuildContext context) async {
     try {
-      final dynamic allMeditations = meditationBox
-          .get("meditation_data"); // if need can use another key
+      final dynamic allMeditations =
+          meditationBox.get("meditation_data"); // if need can use another key
 
       // No need but let do
       // empty list to store data as List of Maps of String & dynamic
@@ -29,7 +29,8 @@ class MeditationServices {
             .map((meditation) => Map<String, dynamic>.from(meditation)));
       }
 
-      meditationList.add(meditation.toJson()); // toJson is in the MeditationExerciseModel
+      meditationList
+          .add(meditation.toJson()); // toJson is in the MeditationExerciseModel
 
       await meditationBox.put("meditation_data", meditationList);
 
@@ -46,6 +47,84 @@ class MeditationServices {
         content: Text("Failed to add a new meditation !"),
         duration: Duration(seconds: 3),
         backgroundColor: Colors.redAccent,
+      );
+    }
+  }
+
+  // Method to get all the meditations
+  List<MeditationExerciseModel> getStoredMeditations() {
+    try {
+      final dynamic allMeditations = meditationBox.get("meditation_data");
+      // If allMeditations is a List, the code iterates over each item in the
+      // list, assuming each item represents a meditation in either Map<String,
+      // dynamic> format or another format that can be cast to Map<String, dynamic>.
+      if (allMeditations != null && allMeditations is List) {
+        return allMeditations.map((meditation) {
+          if (meditation is Map<String, dynamic>) {
+            return MeditationExerciseModel.fromJson(meditation);
+          } else {
+            return MeditationExerciseModel.fromJson(
+                Map<String, dynamic>.from(meditation));
+          }
+        }).toList();
+      } else {
+        // If no data exists, meditationBox.get("meditation_data") returns null.
+        return [];
+      }
+    } catch (err) {
+      // If an error occurs, it's caught, printed as "Services load
+      // error : $err", and the method returns an empty list [].
+      print("Services load error : $err");
+      return [];
+    }
+  }
+
+  // Method to delete a meditation
+  Future<void> deleteMeditationFromStorage(
+      MeditationExerciseModel meditationExercise, BuildContext context) async {
+    try {
+      final dynamic allMeditations = meditationBox.get("meditation_data"); // type of allMeditation is dynamic not Map<String, dynamic>
+
+      if (allMeditations != null && allMeditations is List) {
+        // New list type of Map<String, dynamic>
+        // List.from is a Dart constructor that creates a new List from any given iterable.
+        //By specifying List<Map<String, dynamic>>.from(...), we ensure that the
+        // output list (meditationList) is explicitly of type List<Map<String, dynamic>>.
+        List<Map<String, dynamic>> meditationList =
+            List<Map<String, dynamic>>.from(
+          allMeditations.map( // iterates over each item in allMeditations, applying a transformation function to each item.
+            (meditation) => Map<String, dynamic>.from(meditation),
+          ),
+        );
+        // This transformation is needed because allMeditations contains
+        // dynamic data instead of Map<String, dynamic>
+
+        meditationList.removeWhere((meditation) {
+          MeditationExerciseModel currentMeditation =
+              MeditationExerciseModel.fromJson(
+                  (Map<String, dynamic>.from(meditation)));
+          return currentMeditation.name == meditationExercise.name &&
+              currentMeditation.category == meditationExercise.category;
+        });
+        await meditationBox.put("meditation_data", meditationList);
+
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text("Meditation deleted"),
+              duration: Duration(seconds: 3),
+              backgroundColor: AppColors.primaryBlack,
+            ),
+        );
+      }
+    } catch (err) {
+      print("Service delete error : $err");
+
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Error deleting meditation"),
+            duration: Duration(seconds: 3),
+            backgroundColor: Colors.redAccent,
+          ),
       );
     }
   }
